47b282496f00bec1be812988ca4f578815e879f6
Sindhuja74383 <24149053@drngpit.ac.in>
2026-02-04 12:11:18 +0530

    Create 2317-CountCollisionsOnARoad

---
+++ 2317-CountCollisionsOnARoad/2317-CountCollisionsOnARoad.java
@@ -0,0 +1,12 @@
+// Last updated: 2/4/2026, 12:11:33 PM
+class Solution {
+    public int countCollisions(String directions) {
+    int n = directions.length(), ans = 0, l = 0, r = n - 1; 
+        while(l < n && directions.charAt(l) == 'L') l++;
+        while(r >= l && directions.charAt(r) == 'R') r--;
+        for(int i = l ; i <= r; i++) {
+            if(directions.charAt(i) != 'S') ans++;
+        }
+        return ans;    
+    }
+}
