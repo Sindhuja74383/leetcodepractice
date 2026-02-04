f75a07db36135edf4b17a458e5ffe2aa5dbc213c
Sindhuja74383 <24149053@drngpit.ac.in>
2026-02-04 12:11:30 +0530

    Create 1642-WaterBottles

---
+++ 1642-WaterBottles/1642-WaterBottles.java
@@ -0,0 +1,16 @@
+// Last updated: 2/4/2026, 12:11:44 PM
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+      int drinked = 0;
+        int eb = 0; 
+        
+        while (numBottles > 0) {
+            drinked += numBottles;   
+            eb += numBottles;     
+            
+            numBottles = eb / numExchange; 
+            eb = eb % numExchange;         
+        }
+        return drinked;  
+    }
+}
