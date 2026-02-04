433991c7621f43bc51f6c6cc1ad982915d0aacf3
Sindhuja74383 <24149053@drngpit.ac.in>
2026-02-04 12:11:08 +0530

    Create 2576-MinimumPenaltyForAShop

---
+++ 2576-MinimumPenaltyForAShop/2576-MinimumPenaltyForAShop.java
@@ -0,0 +1,18 @@
+// Last updated: 2/4/2026, 12:11:23 PM
+class Solution {
+    public int bestClosingTime(String customers) {
+    int penalty = 0, best_penalty = 0, ans = 0;
+        char[] ch = customers.toCharArray();
+        for(int i = 0; i < customers.length(); i++) {
+            if(ch[i] == 'Y') {
+                penalty--;
+                if(penalty < best_penalty) {
+                    best_penalty = penalty;
+                    ans = i + 1;
+                }
+            }
+            else penalty++;
+        }
+        return ans;    
+    }
+}
